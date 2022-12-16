24ed5f669a6d670c7d845341598c1a0dfe064b7e
nilssvensson <nils27school@gmail.com>
2022-12-16 13:38:04 -0500

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import LoginForm from "./components/loginform";
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (forName) => {
+    setCurrentForm(forName);
+  }
+
   return (
-    <div className="page">
-      <LoginForm />
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
+      }
     </div>
   );
 }
